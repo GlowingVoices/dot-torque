fb94f085dd3e887a9e367e51f38a43c75b85a9cf
glowingvoices <glowingvoices@gmail.com>
2023-12-18 13:16:01 -0500

    Update index.js
    Add in the reminder about 'sides', key an event listener to the enter key

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Credit: Mannat V. Jain for creating the website
 
 const finalRPM = document.getElementById("final_rpm");
 const motorCount = document.getElementById("motor_count");
@@ -7,10 +6,20 @@ const runButton = document.getElementById("run");
 // Formula: (210/RPM) * Motor Count
 
 runButton.addEventListener('click', () => {
- if (finalRPM.value && motorCount.value) {
-   let torque = ((210)/finalRPM.value)*(motorCount.value/2);
-   alert('Torque output: ' + Math.round((torque*1000))/1000 + " Newton Meters per side.");
- } else {
-   alert('You need to enter values into both the boxes');
- }
+    submit_input();
 });
+
+document.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        submit_input();
+    }
+});
+
+function submit_input() {
+    if (finalRPM.value && motorCount.value) {
+        let torque = ((210) / finalRPM.value) * (motorCount.value / 2);
+        alert('Torque output: ' + Math.round((torque * 1000)) / 1000 + " Newton Meters per side.");
+    } else {
+        alert('You need to enter values into both the boxes');
+    }
+}
